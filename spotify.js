570eae7de0ed1e9c96f7428741cd39e9a3e61160
Bhavya Antil <bhavyaantil06@gmail.com>
2023-10-01 22:47:09 +0530

    Basically completed the html and css, now adding js to the project

---
+++ spotify.js
@@ -0,0 +1,12 @@
+console.log("Welcome to Spotify")
+
+let songIndex = 0
+let audioElement = new Audio('Kangana Tera Ni - ABEER ARORA _ Laung Mare Lashkare (Full Video)_Full-HD-mc.m4a')
+let masterPlay
+
+let songs = [
+    {songName: "Champagne " , filePath: "CHAMPAGNE- Diljit Dosanjh (Official Audio) Intense _ Raj Ranjodh _ MoonChild Era_Full-HD-mc.m4a", coverPath: "CHAMPAGNE- Diljit Dosanjh (Official Audio) Intense _ Raj Ranjodh _ MoonChild Era_Full-HD-mc.jpg"    }
+]
+// audioElement.play();
+
+// wap to add js to the spotify clone?
